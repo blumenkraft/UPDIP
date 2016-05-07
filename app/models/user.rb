dd94a73445d8fc27f98e90f650b6e61dd52eee4b
Kai Wood <kwood@kwd.io>
2016-05-07 22:02:18 +0200

    Fetch avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  def fetch_avatar
+    self.avatar = open("https://api.app.net/users/@#{self.name}/avatar")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507184528) do
+ActiveRecord::Schema.define(version: 20160507195109) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20160507184528) do
     t.string   "facebook"
     t.string   "bitcoin_address"
     t.string   "appstore_link"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
