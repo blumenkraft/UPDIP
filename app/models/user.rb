7fca3171ec3ba8e17854e7be492f60c2fdbf0ec7
Kai Wood <kwood@kwd.io>
2016-05-12 19:24:02 +0200

    Add some basic layout and structure to the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   has_attached_file :avatar
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
+  before_save :fetch_avatar
+
   def fetch_avatar
-    self.avatar = open("https://api.app.net/users/@#{self.name}/avatar")
+    self.avatar = open("https://api.app.net/users/@#{self.username}/avatar")
   end
 end
