bc1173e1569fc5b5ae73371aa8bd2b9737b2924b
Kai Wood <kwood@kwd.io>
2016-05-12 19:23:43 +0200

    Rename a field

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507195109) do
+ActiveRecord::Schema.define(version: 20160507200346) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "flattr"
     t.string   "twitter"
     t.string   "facebook"
