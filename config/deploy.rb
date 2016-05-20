b3b079a36ccd1edfed8dab719124cb679d883379
Kai Wood <kwood@kwd.io>
2016-05-20 18:07:44 +0200

    Add hook to automatically restart after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,6 +51,8 @@ namespace :deploy do
     end
   end
 
+  after :deploy, 'deploy:restart'
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
