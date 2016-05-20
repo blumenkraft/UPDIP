2a75334e09916198a3ac8110d3f54941b2f0edf7
Kai Wood <kwood@kwd.io>
2016-05-20 18:37:44 +0200

    Link .env file on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :rbenv_roles, :all
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
