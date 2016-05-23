a1cd2e07511b060624c935649b191b968e266475
Kai Wood <kwood@kwd.io>
2016-05-23 19:50:32 +0200

    Add stylish fallback avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar
+  has_attached_file :avatar, default_url: ActionController::Base.helpers.asset_path('/assets/missing.png')
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   before_save :fetch_avatar
